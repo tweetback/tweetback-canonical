e0f5a6c5b91a3e574e2e51167185338754ea4d37
Nicolas Hoizey <nicolas@hoizey.com>
2022-11-21 22:55:50 +0100

    Fix typo

--- mapping.js
+++ mapping.js
@@ -2,6 +2,6 @@
 export const mapping = {
 	"zachleat": "https://www.zachleat.com/twitter/",
 	"eleven_ty": "https://twitter.11ty.dev",
-	"nhoizey": "https://twitter.nicolas-hoizey.com"k,
+	"nhoizey": "https://twitter.nicolas-hoizey.com",
 	"rknightuk": "https://hellsite.rknight.me"
 };
