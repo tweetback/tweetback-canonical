c87516d409cbc5e0b80aef877607e501b38f0546
Steren <steren.giannini@gmail.com>
2022-11-21 22:46:44 -0800

    Add twitter.steren.fr

--- mapping.js
+++ mapping.js
@@ -3,5 +3,6 @@ export const mapping = {
 	"zachleat": "https://www.zachleat.com/twitter/",
 	"eleven_ty": "https://twitter.11ty.dev",
 	"nhoizey": "https://twitter.nicolas-hoizey.com",
-	"rknightuk": "https://hellsite.rknight.me"
+	"rknightuk": "https://hellsite.rknight.me",
+	"steren": "https://twitter.steren.fr"
 };
