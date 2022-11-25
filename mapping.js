a55c3aca005b06655fd4b5a00a8e68888f4830f8
nicole <13684891+phoebethewitch@users.noreply.github.com>
2022-11-24 21:05:13 -0500

    Add cutewitchphoebe

--- mapping.js
+++ mapping.js
@@ -7,5 +7,8 @@ export const mapping = {
 	"rknightuk": "https://hellsite.rknight.me",
 	"steren": "https://twitter.steren.fr",
 	"saneef": "https://tweets.saneef.com/",
-	"accudio": "https://twitter.alistairshepherd.uk"
+	"accudio": "https://twitter.alistairshepherd.uk",
+	"cutewitchphoebe": "https://twitter.catgirlin.space",
+	"madelinecatgirl": "https://twitter.catgirlin.space",
+	"purplevioletsky": "https://twitter.catgirlin.space"
 };
